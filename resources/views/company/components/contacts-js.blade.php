3efdd3848acac240e34c5a34c4dae8cde5af5938
youkiname <youkiname@mail.ru>
2022-04-12 01:34:51 +0300

    Now extra employee's phone and email fields will be insert before add button

--- resources/views/company/components/contacts-js.blade.php
+++ resources/views/company/components/contacts-js.blade.php
@@ -2,24 +2,24 @@
     document.addEventListener('DOMContentLoaded', function(){
         let addNewPhoneButton = document.getElementById('add-new-phone-btn');
         addNewPhoneButton.onclick = function() {
-            $("#personal-phones").append(`
+            $(`
             <div class="contragent-form__item">
                 <label for="employee_phones">Рабочий телефон</label>
                 <input type="tel" name="employee_phones[]">
                 <a href="javascript:void(0)" class="remove"></a>
             </div>
-            `);
+            `).insertBefore( "#add-new-phone-btn" );
         };
         
         let addNewEmailButton = document.getElementById('add-new-email-btn');
         addNewEmailButton.onclick = function() {
-            $("#personal-mails").append(`
+            $(`
             <div class="contragent-form__item">
                 <label for="employee_emails[]">Рабочий e-mail</label>
                 <input type="email" name="employee_emails[]">
                 <a href="javascript:void(0)" class="remove"></a>
             </div>
-            `);
+            `).insertBefore( "#add-new-email-btn" );
         };
 
         $(document).on('click', '.remove', function () {
