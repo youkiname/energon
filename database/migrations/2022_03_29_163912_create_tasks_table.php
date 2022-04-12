4e264e0620a8fbbdb076732ecdb423448eaa8a3d
youkiname <youkiname@mail.ru>
2022-04-12 20:25:01 +0300

    fix bug with nullable company_id in task model

--- database/migrations/2022_03_29_163912_create_tasks_table.php
+++ database/migrations/2022_03_29_163912_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->references('id')->on('companies')->nullable()->onDelete('cascade');;
+            $table->foreignId('company_id')->nullable()->references('id')->on('companies')->onDelete('cascade');
             $table->foreignId('user_id')->references('id')->on('users');
             $table->string('title');
             $table->text('description');
