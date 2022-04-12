b6a742fbc377e34bf91d42d3935812545889d476
youkiname <youkiname@mail.ru>
2022-04-13 02:02:29 +0300

    fix bug with not bundled companies displaying

--- app/Http/Livewire/BundleCompanySelect.php
+++ app/Http/Livewire/BundleCompanySelect.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Company;
+use Illuminate\Support\Facades\DB;
 
 class BundleCompanySelect extends Component
 {
@@ -13,7 +14,12 @@ class BundleCompanySelect extends Component
 
     public function mount($currentCompany)
     {
-        $this->companies = Company::all();
+        // Выбрать все компании, которые ещё не связаны с текущей.
+        $bundleSelect = 'not exists (select 1 from company_bundles where company_bundles.a_company_id = companies.id AND company_bundles.b_company_id = ?)';
+        $this->companies = Company::where('id', '!=', $currentCompany->id)
+        ->whereRaw($bundleSelect, [$currentCompany->id])
+        ->get();
+
         $this->currentCompany = $currentCompany;
     }
 
