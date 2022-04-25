1669c9e4ced3274971e9c074a906fdced7e45d68
youkiname <youkiname@mail.ru>
2022-04-25 21:28:28 +0300

    rename user_id to creator_id in companies db table

--- database/migrations/2022_03_25_165742_create_companies_table.php
+++ database/migrations/2022_03_25_165742_create_companies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('creator_id')->references('id')->on('users');
             $table->string('name');
             $table->string('ssn', 13)->unique();
             $table->string('legal');
